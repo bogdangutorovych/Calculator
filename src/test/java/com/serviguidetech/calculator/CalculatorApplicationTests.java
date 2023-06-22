3e070b7b52c7c56de1727056d363ae122b1cdc2d
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2023-06-22 11:27:34 +0200

    initial commit

---
+++ src/test/java/com/serviguidetech/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviguidetech.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
